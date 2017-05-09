88718f6409ae6776c2abfa3cb24e2481e254a47a
gonzalojpv <gonzalojpv@gmail.com>
2017-05-09 15:27:00 -0500

    GP, Toggle Sidebar

--- src/scenes/Dashboard/components/Header/index.js
+++ src/scenes/Dashboard/components/Header/index.js
@@ -2,15 +2,21 @@ import React from "react";
 import { Breadcrumb  } from 'react-bootstrap'
 import './styles.css';
 
-const Header = () => {
+const Header = ({ toggleSidebar }) => {
   return(
     <header className="main-header">
-      <a href="index2.html" className="logo">
+      <a href="/dashboard" className="logo">
         <span className="logo-mini"><b>Taak</b>In</span>
         <span className="logo-lg"><b>Taak</b>In</span>
       </a>
       <nav className="navbar navbar-static-top">
-        <a href="#" className="sidebar-toggle" data-toggle="offcanvas" role="button">
+        <a href="#"
+          className="sidebar-toggle"
+          data-toggle="offcanvas"
+          role="button"
+          onClick={ () => {
+            toggleSidebar();
+          }}>
           <span className="sr-only">Toggle navigation</span>
         </a>
         <div className="navbar-custom-menu">
--- src/scenes/Dashboard/index.js
+++ src/scenes/Dashboard/index.js
@@ -6,10 +6,29 @@ import './styles.css';
 
 export default class Dashboard extends Component {
 
+  constructor( props ) {
+    super(props);
+    this.state = {
+      toggle:''
+    }
+  }
+
+  toggleSidebar() {
+
+    if ( '' !== this.state.toggle )
+      this.setState({ toggle: '' });
+    else
+      this.setState({ toggle: 'sidebar-open' });
+
+    console.log( this.state.toggle );
+  }
+
   render() {
     return(
-      <div className="wrapper">
-        <Header/>
+      <div className={ 'wrapper ' + this.state.toggle}>
+        <Header
+          toggleSidebar={this.toggleSidebar.bind(this)}
+          />
         <Sidebar/>
         <div className="content-wrapper">
           Content
